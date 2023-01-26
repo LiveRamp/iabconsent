6fd0b2562ce1532e32c612323b518236c1d4b6a7
Kyle Halverson <kyle.halverson@liveramp.com>
2023-01-26 13:41:00 -0500

    [PX-2202] Parse GPP Header feature and tests. (#23)
    * Parse GPP Header feature and tests with matching examples with IABs examples.

---
+++ gpp_parsed_consent.go
@@ -0,0 +1,44 @@
+package iabconsent
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+type GppHeader struct {
+	Type     int
+	Version  int
+	Sections []int
+}
+
+// ParseGppHeader parses the first (and required) part of any GPP Consent String.
+// It is used to read the Type, Version, and which sections are contained in the following string(s).
+// Format is:
+// Type	    Int(6)	Fixed to 3 as “GPP Header field”
+// Version	Int(6)	Version of the GPP spec (version 1, as of Jan. 2023)
+// Sections	Range(Fibonacci)	List of Section IDs that are contained in the GPP string.
+func ParseGppHeader(s string) (*GppHeader, error) {
+	// IAB's base64 conversion means a 6 bit grouped value can be converted to 8 bit bytes.
+	// Any leftover bits <8 would be skipped in normal base64 decoding.
+	// Therefore, pad with 6 '0's w/ `A` to ensure that all bits are decoded into bytes.
+	var b, err = base64.RawURLEncoding.DecodeString(s + "A")
+	if err != nil {
+		return nil, errors.Wrap(err, "parse gpp header consent string")
+	}
+
+	var r = NewConsentReader(b)
+
+	var g = &GppHeader{}
+	g.Type, _ = r.ReadInt(6)
+	if g.Type != 3 {
+		return nil, errors.New("wrong gpp header type " + fmt.Sprint(g.Type))
+	}
+	g.Version, _ = r.ReadInt(6)
+	if g.Version != 1 {
+		return nil, errors.New("unsupported gpp version " + fmt.Sprint(g.Version))
+	}
+	g.Sections, _ = r.ReadFibonacciRange()
+	return g, r.Err
+}
